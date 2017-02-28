dd7742d4b8f2189d8091301e4a6e0a132b01217b
Hu Zeming <1059434300@qq.com>
2017-02-28 22:20:00 +0800

    用户注册添加服务器的代码

--- app/src/main/java/cn/ucai/live/ui/activity/LoginActivity.java
+++ app/src/main/java/cn/ucai/live/ui/activity/LoginActivity.java
@@ -26,7 +26,7 @@ import cn.ucai.live.utils.L;
 import cn.ucai.live.utils.MD5;
 
 /**
- * A login screen that offers login via email/password.
+ * A login screen that offers login via email/etPassword.
  */
 public class LoginActivity extends BaseActivity {
 
@@ -77,7 +77,9 @@ public class LoginActivity extends BaseActivity {
         startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
       }
     });
-
+if (LiveHelper.getInstance().getCurrentUsernName()!=null){
+  mEmailView.setText(LiveHelper.getInstance().getCurrentUsernName());
+}
 
   }
 
@@ -99,7 +101,7 @@ public class LoginActivity extends BaseActivity {
     boolean cancel = false;
     View focusView = null;
 
-    // Check for a valid password, if the user entered one.
+    // Check for a valid etPassword, if the user entered one.
     if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
       mPasswordView.setError(getString(R.string.error_invalid_password));
       focusView = mPasswordView;
